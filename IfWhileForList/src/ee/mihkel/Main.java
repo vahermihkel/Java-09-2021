abb856ee99fe526f8ac3d4f5794cb9f6c7022397
vahermihkel <vahermihkel@gmail.com>
2021-09-07 18:34:49 +0300

    ifwhileforlist

---
+++ IfWhileForList/src/ee/mihkel/Main.java
@@ -0,0 +1,30 @@
+package ee.mihkel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Töötab");
+
+//        while(false) {
+//            System.out.println("Tere");
+//        }
+
+
+        List<String> massiiv = new ArrayList<>();
+        massiiv.add("Üks");
+        massiiv.add("Kaks");
+        massiiv.add("Kolm");
+
+        for (int i = 0; i < massiiv.size(); i++) {
+            System.out.println(i + massiiv.get(i));
+        }
+
+        for ( String s : massiiv) {
+            System.out.println(s); // sout
+        }
+
+    }
+}
